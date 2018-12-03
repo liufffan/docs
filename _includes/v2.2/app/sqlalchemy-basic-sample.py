9ba0c43aa5fdd71785e3f5e9b36228b86ccc68ff
Rich Loveland <rich@cockroachlabs.com>
2018-12-03 10:40:01 -0500

    WIP

--- _includes/v2.2/app/sqlalchemy-basic-sample.py
+++ _includes/v2.2/app/sqlalchemy-basic-sample.py
@@ -1,3 +1,177 @@
+import random
+from math import floor
+from sqlalchemy import create_engine, Column, Integer
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from cockroachdb.sqlalchemy import run_transaction
+
+Base = declarative_base()
+
+VERBOSE = False  # Set to True for printed summaries of account balances
+
+
+# The Account class corresponds to the "accounts" database table.
+class Account(Base):
+    __tablename__ = 'accounts'
+    id = Column(Integer, primary_key=True)
+    balance = Column(Integer)
+
+
+# Create an engine to communicate with the database. The
+# "cockroachdb://" prefix for the engine URL indicates that we are
+# connecting to CockroachDB using the 'cockroachdb-python' dialect.
+# For more information, see
+# https://github.com/cockroachdb/cockroachdb-python.
+
+engine = create_engine(
+    'cockroachdb://maxroach@localhost:26257/bank',
+    connect_args={
+        'sslmode': 'require',
+        'sslrootcert': 'certs/ca.crt',
+        'sslkey': 'certs/client.maxroach.key',
+        'sslcert': 'certs/client.maxroach.crt',
+    },
+    echo=True                   # Set to True to log SQL queries to stdout
+)
+
+Sessionmaker = sessionmaker(bind=engine)
+
+# Automatically create the "accounts" table based on the Account class.
+Base.metadata.create_all(engine)
+
+# Insert rows into the "accounts" table.  The code below makes
+# sure this script (1) checks the DB for existing IDs, and (2)
+# generates random IDs for new accounts, while ensuring the new IDs
+# don't collide with any existing ones.
+
+session = Sessionmaker()
+
+seen = {}
+
+# Get and store existing account IDs for later collision checks during
+# random account ID creation.
+
+res = engine.execute('SELECT id from accounts;')
+for elem in res:
+    val = elem[0]
+    if val not in seen:
+        seen[val] = 1
+
+# Create new accounts with random IDs and random account balances.
+
+new_accounts = []
+elems = iter(range(100))
+for i in elems:
+    maybe_id = floor(random.random()*1_000_000)
+    if maybe_id not in seen:
+        new_accounts.append(
+            Account(
+                id=maybe_id,
+                balance=floor(random.random()*1_000_000)
+            )
+        )
+        seen[maybe_id] = 1
+    else:
+        # Skip account IDs that already exist.
+        next(elems)
+
+session.add_all(new_accounts)
+session.commit()
+
+
+# Helper for getting random value from the seen hash
+def get_random_account_id():
+    # https://www.pythoncentral.io/select-random-item-list-tuple-data-structure-python/
+    return random.choice(list(seen.keys()))
+
+
+# The following functions are the transaction(s) we may want to run
+# using `run_transaction`.
+
+def transfer_funds(session, frm, to, amount):
+    """Transfer AMOUNT funds between accounts FRM and TO (during SESSION).
+
+    TODO: Consider updating this to accept account objects as args
+    since we're doing duplicate work when called by
+    TRANSFER_FUNDS_RANDOMLY.
+    """
+    source = session.query(Account).filter_by(id=frm).one()
+    sink = session.query(Account).filter_by(id=to).one()
+
+    if source.balance < amount:
+        raise "Insufficient funds"
+
+    source.balance -= amount
+    session.query(Account).filter_by(id=to).update(
+        {"balance": (Account.balance + amount)}
+    )
+    if VERBOSE:
+        print("Balance after:  id {} => ${}, id {} => ${}"
+              .format(source.id, source.balance, sink.id, sink.balance))
+
+
+def transfer_funds_randomly(session):
+    """Transfer money randomly between accounts (during SESSION).
+
+    Just for fun, cut a randomly selected account's balance in half,
+    and give that half to some other randomly selected account.
+    """
+    frm_id = get_random_account_id()
+    to_id = get_random_account_id()
+    if VERBOSE:
+        print("from => {}, to => {}".format(frm_id, to_id))
+    frm_acc = session.query(Account).filter_by(id=frm_id).one()
+    to_acc = session.query(Account).filter_by(id=to_id).one()
+    amt_to_transfer = floor(frm_acc.balance/2)
+    if VERBOSE:
+        print("Balance before: id {} => ${}, id {} => ${}"
+              .format(frm_id, frm_acc.balance, to_id, to_acc.balance))
+    transfer_funds(session, frm_id, to_id, amt_to_transfer)
+
+
+# Transfer funds (randomly) between accounts.
+
+# Note: the CockroachDB dialect's driver *must* be passed a
+# sessionmaker object (instead of a session, like most other dialects)
+# to ensure that a new session is created for each transaction.  This
+# is necessary because, in order to support the ANSI isolation level
+# SERIALIZABLE, CockroachDB needs to retry transactions more often
+# than other databases that default to less safe isolation levels.
+# Note that this need for additional transaction retries is a
+# characteristic of SERIALIZABLE isolation and not specific to
+# CockroachDB's implementation -- it's inherent in the model, since
+# SERIALIZABLE implies that transactions will be "pushed" more often
+# to ensure correct ordering, and will thus need to be retried more
+# often by clients.
+
+# Note: don't use
+# [Session.flush()](https://docs.sqlalchemy.org/en/latest/orm/session_api.html#sqlalchemy.orm.session.Session.flush)
+# inside CockroachDB transactions!  Even though `flush()` is not
+# technically considered a "session lifecycle method", it relies on
+# the underlying database supporting nested transactions.  if the call
+# to `flush()` fails,
+# you will likely get an error like the following:
+
+# sqlalchemy.exc.NotSupportedError: (psycopg2.NotSupportedError) SAVEPOINT not supported except for COCKROACH_RESTART
+#  [SQL: 'ROLLBACK TO SAVEPOINT sa_savepoint_1'] (Background on this error at: http://sqlalche.me/e/tw8g)
+
+# This happens because CockroachDB does not have support for nested
+# transactions (a.k.a. subtransactions), nor renaming savepoints (only
+# the one is supported), so if the call to `flush()` fails and you are
+# working with objects which are also using the same Session (xxx: use
+# full name) object to store their state, you may see an error like:
+
+# sqlalchemy.orm.exc.DetachedInstanceError: Instance <FooObjectModel at 0x7f49c5a0c0b8> is not bound to a Session; attribute refresh operation cannot proceed (Background on this error at: http://sqlalche.me/e/bhk3)```
+
+run_transaction(Sessionmaker, transfer_funds_randomly)
+
+# Print out the balances.
+if VERBOSE:
+    for account in session.query(Account):
+        print(account.id, account.balance)
+
+
+
 from __future__ import print_function
 from sqlalchemy import create_engine, Column, Integer
 from sqlalchemy.ext.declarative import declarative_base
--- v2.2/build-a-python-app-with-cockroachdb-sqlalchemy.md
+++ v2.2/build-a-python-app-with-cockroachdb-sqlalchemy.md
@@ -23,9 +23,7 @@ For a more realistic use of SQLAlchemy with CockroachDB, see our [`examples-orms
 {% include {{page.version.version}}/app/before-you-begin.md %}
 
 {{site.data.alerts.callout_danger}}
-
 **Upgrading from CockroachDB 2.0 to 2.1?** If you used SQLAlchemy with your 2.0 cluster, you must [upgrade the adapter to the latest release](https://github.com/cockroachdb/cockroachdb-python) before upgrading to CockroachDB 2.1.
-
 {{site.data.alerts.end}}
 
 ## Step 1. Install the SQLAlchemy ORM
@@ -56,15 +54,32 @@ $ cockroach cert create-client maxroach --certs-dir=certs --ca-key=my-safe-direc
 
 ## Step 3. Run the Python code
 
-The following code uses the [SQLAlchemy ORM](https://docs.sqlalchemy.org/en/latest/) to map Python-specific objects to SQL operations. Specifically, `Base.metadata.create_all(engine)` creates an `accounts` table based on the Account class, `session.add_all([Account(),...
+The following code uses the [SQLAlchemy ORM](https://docs.sqlalchemy.org/en/latest/) to map Python objects and methods to SQL operations. 
+
+Specifically, `Base.metadata.create_all(engine)` creates an `accounts` table based on the Account class, `session.add_all([Account(),...
 ])` inserts rows into the table, and `session.query(Account)` selects from the table so that balances can be printed.
 
 {{site.data.alerts.callout_info}}
-The <a href="https://github.com/cockroachdb/cockroachdb-python">cockroachdb python package</a> installed earlier is triggered by the <code>cockroachdb://</code> prefix in the engine URL. Using <code>postgres://</code> to connect to your cluster will not work.
+Use of the [`cockroachdb-python` package](https://github.com/cockroachdb/cockroachdb-python") is triggered by the `cockroachdb://` prefix in the engine URL shown below. Using `postgres://` to connect to your CockroachDB cluster will not work.
 {{site.data.alerts.end}}
 
 Copy the code or
 <a href="https://raw.githubusercontent.com/cockroachdb/docs/master/_includes/v2.2/app/sqlalchemy-basic-sample.py" download>download it directly</a>.
+
+The script below does a number of things.  At a high level, it:
+
+1. Reads the existing account IDs.
+2. Creates a number of accounts with random IDs and adds a randomly generated amount of money to each account.  (Step #1 was necessary to avoid collisions during the account creation in this step.)
+3. Chooses two accounts at random from the list of accounts and takes half of the money from the first, and deposits it into the second.
+
+It does all of the above using the recommended practices for using SQLAlchemy with the CockroachDB dialect's driver, which are (but are not limited to):
+
+- DO use the `run_transaction` interface defined by the driver, which abstracts the details of [transaction retries](XXX) away from you having to care about them, but which are more frequent than in some other database for reasons which are described below.
+- DON'T do any explicit munging of the transaction state inside the callback function passed to `run_transaction` - this includes calls to [`Session.flush`](XXX), which does not work as expected with CockroachDB because CockroachDB does not support nested transactions (subtransactions) or named savepoints, both of which are necessary for the `flush()` method to work properly.
+- DO follow the recommendation of the [SQLAlchemy FAQs](https://docs.sqlalchemy.org/en/latest/orm/session_basics.html#session-frequently-asked-questions), which affirm the previous point by stating (among other things) that
+> As a general rule, the application should manage the lifecycle of the session externally to functions that deal with specific data. This is a fundamental separation of concerns which keeps data-specific operations agnostic of the context in which they access and manipulate that data.
+> Keep the lifecycle of the session (and usually the transaction) separate and external
+
 
 {% include copy-clipboard.html %}
 ~~~ python
